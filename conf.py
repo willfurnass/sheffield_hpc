9343818f12627a29f4775c2883b0c9391a17cc5c
Stuart Mumford <stuart@mumford.me.uk>
2015-04-15 10:03:21 +0100

    more bootstrap2 updates

--- conf.py
+++ conf.py
@@ -61,7 +61,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'iceberg Software'
+project = u'iceberg Documentation'
 copyright = u'2015, CICS Sheffield'
 author = u'CICS Sheffield'
 
@@ -163,11 +163,11 @@ todo_include_todos = False
 
 html_theme_path = ['themes/sphinx-bootstrap/sphinx-bootstrap', 'themes/sphinx-bootstrap/']
 html_theme = 'sphinx-bootstrap'
-
+html_sidebars = { '**': ['globaltoc.html', 'localtoc.html'] }
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'iceberg Software'
+html_title = 'iceberg Documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -197,54 +197,7 @@ html_static_path = ['_static']
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-#html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-#html_additional_pages = {}
-
-# If false, no module index is generated.
-#html_domain_indices = True
-
-# If false, no index is generated.
-#html_use_index = True
-
-# If true, the index is split into individual pages for each letter.
-#html_split_index = False
-
-# If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-#html_use_opensearch = ''
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
-
-# Language to be used for generating the HTML full-text search index.
-# Sphinx supports the following languages:
-#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
-#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
-
-# A dictionary with options for the search language support, empty by default.
-# Now only 'ja' uses this config value
-#html_search_options = {'type': 'default'}
-
-# The name of a javascript file (relative to the configuration directory) that
-# implements a search results scorer. If empty, the default will be used.
-#html_search_scorer = 'scorer.js'
+html_use_smartypants = True
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'icebergSoftwaredoc'
--- index.rst
+++ index.rst
@@ -1,5 +1,9 @@
-iceberg Software
-================
+iceberg Documentation
+=====================
+
+Iceberg is the University of Sheffield's centralised High Perfomance Computing 
+(HPC) resource. Its mission is to meet the intensive computing requirements of 
+research projects at Sheffield University.
 
 Contents:
 
@@ -7,4 +11,4 @@ Contents:
    :maxdepth: 1
    :glob:
    
-   software/*
+   software/index
