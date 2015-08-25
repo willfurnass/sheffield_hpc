e8f549a50fdb4f063066010d99db111f041e2577
Mike Croucher <Michael.P.Croucher@googlemail.com>
2015-08-25 16:57:24 +0100

    Module and install scripts for PGI version of netcdf

---
+++ software/install_scripts/libs/pgi/netcdf/install_pgi_netcdf_4.3.3.1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+module load compilers/pgi/15.7
+module load mpi/pgi/openmpi/1.8.8
+module load libs/hdf5/pgi/1.8.15-patch1
+
+install_dir=/usr/local/packages6/libs/pgi/netcdf/4.3.3.1
+mkdir -p $install_dir
+
+tar -xzf netcdf-c-4.3.3.1.tar.gz
+cd netcdf-c-4.3.3.1
+
+env CPP=cpp CC=mpicc CXX=mpicxx FC=mpif90 F90=mpif90 LD=mpif90 CPPFLAGS="-DpgiFortran" ./configure --prefix=$install_dir
+
+make 2>&1 | tee make.log
+make check 2>&1 | tee make-check.log
+make install 2>&1 | tee make-install.log
