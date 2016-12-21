fe0dca3ef534d7b87a503e5bb0adbb0431fed411
Will Furnass <will@thearete.co.uk>
2016-12-21 12:19:35 +0000

    ShARC + GULP: minor fixes to install script

--- sharc/software/install_scripts/apps/gulp/4.4/intel-17.0.0/install.sh
+++ sharc/software/install_scripts/apps/gulp/4.4/intel-17.0.0/install.sh
@@ -6,20 +6,20 @@
 ################################################################################
 # Variables
 ################################################################################
-vers=4.4
-tarball_path=/usr/local/media/protected/GULP/${vers}/gulp-${vers}.tgz
-compiler_vers=17.0.0
-openmpi_vers=2.0.1
+VERS=4.4
+TARBALL_PATH=/usr/local/media/protected/GULP/${VERS}/gulp-${VERS}.tgz
+COMPILER_VERS=17.0.0
+OPENMPI_VERS=2.0.1
 
-serial_build_dir=${TMPDIR-/tmp}/$USER/gulp/$vers/intel-${compiler_vers}/serial
-serial_build_config=$(realpath getmachine_serial)
-serial_prefix=/usr/local/packages/apps/gulp/${vers}/intel-${compiler_vers}
+SERIAL_BUILD_DIR=${TMPDIR-/tmp}/$USER/gulp/$VERS/intel-${COMPILER_VERS}/serial
+SERIAL_BUILD_CONFIG=$(realpath getmachine_serial)
+SERIAL_PREFIX=/usr/local/packages/apps/gulp/${VERS}/intel-${COMPILER_VERS}
 
-mpi_build_dir=${TMPDIR-/tmp}/$USER/gulp/$vers/intel-${compiler_vers}/mpi
-mpi_build_config=$(realpath getmachine_mpi)
-mpi_prefix=/usr/local/packages/apps/gulp/${vers}/intel-${compiler_vers}-openmpi-${openmpi_vers}
+MPI_BUILD_DIR=${TMPDIR-/tmp}/$USER/gulp/$VERS/intel-${COMPILER_VERS}/mpi
+MPI_BUILD_CONFIG=$(realpath getmachine_mpi)
+MPI_PREFIX=/usr/local/packages/apps/gulp/${VERS}/intel-${COMPILER_VERS}-openmpi-${OPENMPI_VERS}
 
-workers=16
+#WORKERS=${OMP_NUM_THREADS-1}
 
 ################################################################################
 # Error handling
@@ -34,49 +34,53 @@ handle_error () {
 trap handle_error ERR
 
 ################################################################################
-# Serial build
+# Load common modules
 ################################################################################
 module purge
-module load dev/intel-compilers/17.0.0
+module load dev/intel-compilers/${COMPILER_VERS}
 module load libs/intel-mkl/2017.0/binary
 
-mkdir -m 0700 -p $serial_build_dir
-mkdir -m 2770 -p $serial_prefix
+################################################################################
+# Serial build
+################################################################################
 
-tar -zxf $tarball_path -C $serial_build_dir
-pushd $serial_build_dir/gulp-${vers}/Src
+mkdir -m 0700 -p $SERIAL_BUILD_DIR
+mkdir -m 2770 -p $SERIAL_PREFIX
+
+tar -zxf $TARBALL_PATH -C $SERIAL_BUILD_DIR
+pushd $SERIAL_BUILD_DIR/gulp-${VERS}/Src
 # Create backup of config file
 [[ -f getmachine.orig ]] || cp getmachine{,.orig}
 # Copy config file for serial build in place
-cp $serial_build_config getmachine
-make -j $workers
+cp $SERIAL_BUILD_CONFIG getmachine
+make #-j $WORKERS
 
-cp -r $serial_build_dir/gulp-${vers}/* $serial_prefix/
-mkdir $serial_prefix/bin
-ln -sr $serial_prefix/Src/gulp -t $serial_prefix/bin/
+cp -r $SERIAL_BUILD_DIR/gulp-${VERS}/* $SERIAL_PREFIX/
+mkdir -p $SERIAL_PREFIX/bin
+ln -srf $SERIAL_PREFIX/Src/gulp -t $SERIAL_PREFIX/bin/
 
 ################################################################################
 # MPI build
 ################################################################################
-module load mpi/openmpi/2.0.1/intel-17.0.0
+module load mpi/openmpi/${OPENMPI_VERS}/intel-${COMPILER_VERS}
 
-mkdir -m 0700 -p $mpi_build_dir
-mkdir -m 2770 -p $mpi_prefix
+mkdir -m 0700 -p $MPI_BUILD_DIR
+mkdir -m 2770 -p $MPI_PREFIX
 
-tar -zxf $tarball_path -C $mpi_build_dir
-pushd $mpi_build_dir/gulp-${vers}/Src
+tar -zxf $TARBALL_PATH -C $MPI_BUILD_DIR
+pushd $MPI_BUILD_DIR/gulp-${VERS}/Src
 # Create backup of config file
 [[ -f getmachine.orig ]] || cp getmachine{,.orig}
 # Copy config file for MPI build in place
-cp $mpi_build_config getmachine
-make -j $workers
+cp $MPI_BUILD_CONFIG getmachine
+make #-j $WORKERS
 
-cp -r $mpi_build_dir/gulp-${vers}/* $mpi_prefix/
-mkdir $mpi_prefix/bin
-ln -sr $mpi_prefix/Src/gulp -t $mpi_prefix/bin/
+cp -r $MPI_BUILD_DIR/gulp-${VERS}/* $MPI_PREFIX/
+mkdir -p $MPI_PREFIX/bin
+ln -srf $MPI_PREFIX/Src/gulp -t $MPI_PREFIX/bin/
 
 ################################################################################
 # Restrict permissions
 ################################################################################
-chmod -R o-rwx $serial_prefix $mpi_prefix
-echo "Now, run the following: sudo chgrp -R gulp $serial_prefix $mpi_prefix"
+chmod -R o-rwx $SERIAL_PREFIX $MPI_PREFIX
+echo "Now, run the following: sudo chgrp -R gulp $SERIAL_PREFIX $MPI_PREFIX"
