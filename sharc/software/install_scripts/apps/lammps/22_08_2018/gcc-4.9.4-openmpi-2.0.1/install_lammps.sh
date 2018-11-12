3227f22b26fcb35df20729290622fbdf3ce49cfa
Des Ryan <des.ryan@sheffield.ac.uk>
2018-11-12 16:02:53 +0000

    rename directories to highlight openmpi build

--- sharc/software/install_scripts/apps/lammps/22_08_2018/gcc-4.9.4-openmpi-2.0.1/install_lammps.sh
+++ sharc/software/install_scripts/apps/lammps/22_08_2018/gcc-4.9.4-openmpi-2.0.1/install_lammps.sh
@@ -5,6 +5,6 @@ mkdir build
 cd build
 module load dev/cmake/3.7.1/gcc-4.9.4
 module load mpi/openmpi/2.0.1/gcc-4.9.4
-cmake -C ../cmake/presets/std_nolib.cmake -D CMAKE_INSTALL_PREFIX=/usr/local/packages/apps/lammps/22/gcc-4.9.4/ BUILD_MPI=yes ../cmake
+cmake -C ../cmake/presets/std_nolib.cmake -D CMAKE_INSTALL_PREFIX=/usr/local/packages/apps/lammps/22_08_2018/gcc-4.9.4-openmpi-2.0.1/ BUILD_MPI=yes ../cmake
 make
 make install
