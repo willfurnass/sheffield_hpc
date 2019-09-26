74a1f020e01084703fc5bd18a7a68d98b4c69bd4
Des Ryan <des.ryan@sheffield.ac.uk>
2019-09-26 14:45:47 +0100

    lammps rebuild

--- sharc/software/install_scripts/apps/lammps/22_08_2018/gcc-4.9.4-openmpi-2.0.1/install_lammps.sh
+++ sharc/software/install_scripts/apps/lammps/22_08_2018/gcc-4.9.4-openmpi-2.0.1/install_lammps.sh
@@ -5,6 +5,9 @@ mkdir build
 cd build
 module load dev/cmake/3.7.1/gcc-4.9.4
 module load mpi/openmpi/2.0.1/gcc-4.9.4
-cmake -C ../cmake/presets/std_nolib.cmake -D CMAKE_INSTALL_PREFIX=/usr/local/packages/apps/lammps/22_08_2018/gcc-4.9.4-openmpi-2.0.1/ BUILD_MPI=yes ../cmake
+module load libs/blas/3.4.2-5/binary
+module load apps/ffmpeg/4.1/gcc-4.9.4
+module load libs/lapack/3.4.2-5/binary
+cmake -C ../cmake/presets/manual_selection.cmake -D CMAKE_INSTALL_PREFIX=/usr/local/packages/apps/lammps/22_08_2018/gcc-4.9.4-openmpi-2.0.1/ BUILD_MPI=yes ../cmake
 make
 make install
