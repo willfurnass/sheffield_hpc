65812656db9f37674d9be6d6b83bc63f01cd4619
James Moore <69149348+jkwmoore@users.noreply.github.com>
2021-01-06 15:11:43 +0000

    ShARC R GCC (not Intel) documentation update
    Install scripts, module files added.
    
    Slight modification to the docs to remove latest version can be loaded with section to avoid people using incorrect versions.
    
    .version file added to the R module top directory to ping this at version 3.6.4 to keep using this version while version 4.0.0 modules are tested by end users.

---
+++ sharc/software/install_scripts/apps/R/4.0.0/gcc-10.1.0/install-R4.0-gcc-10.1.0.sh
@@ -0,0 +1,44 @@
+##!/bin/bash -e
+##$ -l rmem=40G
+
+#Set up environment variables and create directories
+version=4.0.0
+install_dir=/usr/local/packages/apps/R/$version/gcc-10.1/
+build_dir=~/R-$version-build
+mkdir -p $build_dir
+mkdir -p $install_dir
+module load dev/gcc/10.1
+cd $build_dir
+
+#Download the installer
+wget https://cran.r-project.org/src/base/R-4/R-$version.tar.gz
+tar -xzf ./R-$version.tar.gz
+cd R-$version
+
+tcl_config=/usr/local/packages/apps/tcl/8.6.10/gcc-10.1/lib/tclConfig.sh
+tk_config=/usr/local/packages/apps/tk/8.6.10/gcc-10.1/lib/tkConfig.sh
+
+#Configure and build
+./configure --prefix $install_dir --with-tcltk --with-tcl-config=$tcl_config \
+ --with-tk-config=$tk_config --with-blas --with-lapack \
+--enable-R-shlib 2>&1 | tee config-R-$version.log
+make 2>&1 | tee make-R-$version.log
+
+#Check
+make check 2>&1 | tee make_check-R-$version.log
+
+#Install test directory
+make install-tests
+
+#Install
+make install
+
+#Build libRmath.so
+cd $build_dir/R-$version/src/nmath/standalone
+make 
+mv libRmath.* $install_dir/lib64/R/lib
+
+#Copy install,configure and test log files
+mkdir -p $install_dir/install_logs
+cd $build_dir/R-$version
+mv ./*.log $install_dir/install_logs
