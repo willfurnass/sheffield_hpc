75c3e747c97b74f8c7e8315bc2336e1f08f0a800
James Moore <69149348+jkwmoore@users.noreply.github.com>
2021-02-23 12:42:17 +0000

    Bedtools docs initial commit.
    Adds documentation for Bedtools on both clusters.

---
+++ bessemer/software/apps/bedtools.rst
@@ -0,0 +1,38 @@
+Bedtools
+========
+
+.. sidebar:: Bedtools
+
+   :Versions:  2.29.2
+   :Dependencies: GCC/9.3.0, XZ/5.2.5-GCCcore-9.3.0, zlib/1.2.11-GCCcore-9.3.0, bzip2/1.0.8-GCCcore-9.3.0, BamTools/2.5.1-GCC-9.3.0
+   :URL: https://bedtools.readthedocs.org/en/latest/
+
+Collectively, the bedtools utilities are a swiss-army knife of tools for a wide-range of genomics analysis tasks.
+
+Interactive usage
+-----------------
+After connecting to Bessemer (see :ref:`ssh`),  start an interactive session with the :code:`qrshx` command.
+
+The latest version of bedtools (currently version 2.29.2) is made available with the command:
+
+.. code-block:: none
+
+        module load BEDTools/2.29.2-GCC-9.3.0
+
+
+After this any of the bedtools commands can be run from the prompt.
+
+
+
+Installation notes
+------------------
+Bedtools using Easybuild, build details can be found in ``/usr/local/packages/live/eb/BEDTools/2.29.2-GCC-9.3.0/easybuild/``
+
+
+Testing
+-------
+See: https://github.com/rcgsheffield/sheffield_hpc/issues/1157
+
+Modulefile
+----------
+The module file is on the system at :download:`/usr/local/packages/live/eb/BEDTools/2.29.2-GCC-9.3.0/easybuild/ </bessemer/software/modulefiles/BEDTools/2.29.2-GCC-9.3.0>`.
---
+++ sharc/software/install_scripts/apps/bedtools/2.30.0/gcc-8.2-cmake-3.17.1/install_bedtools.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+#$ -V
+#$ -cwd
+#$ -M joe.bloggs@sheffield.ac.uk
+#$ -m abe
+#$ -l h_rt=08:00:00
+#$ -l rmem=2G
+#$ -pe smp 8
+#$ -N Bedtools_install
+
+#Naturally this script is for use on SGE clusters only.
+#Suggested resource values above should be sane and work.
+#Script written by JMoore 22-02-2021 @ The University of Sheffield IT Services.
+
+#Notes:
+#  https://github.com/arq5x/bedtools2/releases/download/v2.30.0/bedtools-2.30.0.tar.gz
+
+
+########################  Options and version selections  #########################
+###################################################################################
+
+
+#Define Names and versions
+PACKAGENAME=bedtools
+GCCVER=8.2
+CMAKEVER=3.17.1
+PACKAGEVER=2.30.0
+
+
+######################## You should not need to edit below ########################
+###################################################################################
+
+#Start the main work of the script
+echo "Running install script to make Package: " + $PACKAGENAME " Version: " + $PACKAGEVER
+
+
+#Setup calculated variables
+INSTALLDIR=/usr/local/packages/apps/$PACKAGENAME/$PACKAGEVER/gcc-$GCCVER-cmake-$CMAKEVER/
+SOURCEDIR=$INSTALLDIR/src
+
+MODULEDIR=/usr/local/modulefiles/apps/$PACKAGENAME/$PACKAGEVER/
+MODULEFILENAME=gcc-$GCCVER-cmake-$CMAKEVER
+
+
+FILENAME=$PACKAGENAME-$PACKAGEVER.tar.gz
+URL=https://github.com/arq5x/bedtools2/releases/download/v$PACKAGEVER/$FILENAME
+
+
+#This is used at the end to copy the install script to the install directory if the script succeeds.
+#This may be over-engineered so no matter how this script is called this copy works...
+
+#If you are sourcing this script (why???) you may wish to amend this with $BASH_SOURCE or ${BASH_SOURCE[0]}
+#See: https://stackoverflow.com/a/55798664
+
+SCRIPTDIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPTNAME=${0}
+FULLPATH=$SCRIPTDIR/$SCRIPTNAME
+
+
+#Load Modules
+module load dev/gcc/$GCCVER
+module load dev/cmake/$CMAKEVER/gcc-$GCCVER
+
+echo "Loaded Modules: " + $LOADEDMODULES
+
+
+#Make directories
+mkdir -p $INSTALLDIR
+mkdir -p $SOURCEDIR
+
+echo "Install Directory: " + $INSTALLDIR
+echo "Source Directory: " + $SOURCEDIR
+
+
+#Go to the source directory
+cd $SOURCEDIR
+
+
+#Download source and extract
+echo "Download Source"
+
+wget $URL
+tar -zxvf $FILENAME
+cd bedtools2
+
+#Replace the install dir of /usr/local/ path with our Install path
+sed -i "s#/usr/local#$INSTALLDIR#g" Makefile
+
+
+#Clean
+echo "Make precleaning:"
+
+make -j $NSLOTS clean
+
+
+#Make
+echo "Making:"
+
+make -j $NSLOTS
+
+
+#Check
+echo "Make checking:"
+
+#make -j check
+
+echo "Skipping - uncomment if desired."
+
+
+#Install
+echo "Make installing:"
+
+make -j $NSLOTS install
+
+
+#Echo the loaded modules used to compile the installed directory
+#Splits on colon sends each element to new line
+echo $LOADEDMODULES | awk 'BEGIN{RS=":"}{$1=$1}1' >> $INSTALLDIR/compiler_loaded_modules_list
+
+#Copy the used install script to install directory
+cp $FULLPATH $INSTALLDIR
+
+#To be added - automatic module file generation - usage of EOF likely.
+mkdir -p $MODULEDIR
+touch $MODULEDIR$MODULEFILENAME
