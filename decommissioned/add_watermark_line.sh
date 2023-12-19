5a35110b00dc09be9263d8fa2ea92c7a210d7f99
James Moore <j.k.moore@sheffield.ac.uk>
2023-12-19 15:59:23 +0000

    Make scripts less brittle.

--- decommissioned/add_watermark_line.sh
+++ decommissioned/add_watermark_line.sh
@@ -4,11 +4,19 @@
 line1=".. include:: /referenceinfo/imports/decommissioned/decom_watermark.rst"
 
 # Find all .rst files recursively and add the lines if not present
-find . -type f -name "*.rst" -exec sh -c "
-    # Check if each line is already present
-    if ! grep -q \"$line1\" \"\$0\"; then
-        # Add the first line to the beginning of the file
-        echo \"MISSING LINE 1 in \$0\ \"
-        awk -v line1=\"$line1\" 'BEGIN {print line1 \"\\n\"} {print}' \"\$0\" > temp_file && mv temp_file \"\$0\"
+find . -type f -name "*.rst" -print | while read -r file; do
+    # Check if the line is already present
+    if ! grep -q "$line1" "$file"; then
+        echo "MISSING LINE 1 in $file"
+        # Find the last line starting and ending with ":" to avoid issues with :orphan: etc...
+        lastColonLine=$(grep -n '^:.*:$' "$file" | tail -n 1 | cut -d ":" -f 1)
+        
+        awk -v line1="$line1" -v lastColonLine="$lastColonLine" '
+            {
+                print;
+                if (NR == lastColonLine) {
+                    print "\n" line1 "\n";
+                }
+            }' "$file" > temp_file && mv temp_file "$file"
     fi
-" {} \;
+done
